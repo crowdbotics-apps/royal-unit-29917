6b10e1bd183bae5893f5e7da3057f8eb23cedfa9
Crowdbotics <team@crowdbotics.com>
2021-08-18 17:50:17 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,6 @@
+export const globalOptions = {
+  name: "royal_unit_29917Identifier",
+  url: "https://royal_unit_29917Identifier.botics.co",
+  api: "https://royal_unit_29917Identifier.botics.co/api/v1"
+}
+export const modulesOptions = {}
